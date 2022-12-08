36537a48b7a3ccab02313c5f839c43c7e50dcf2a
Evli <ck18551458185@163.com>
2022-12-08 15:04:25 +0800

    question-new

---
+++ src/api/hmmm/directorys.js
@@ -0,0 +1,41 @@
+/*
+ * @Author: zhanglianchang
+ * @Date: 2020-08-01 14:39:32
+ * @Last Modified by: taoshiwei
+ * @Last Modified time: 2019-04-29 14:56:00
+ *
+ * 目录
+ */
+
+import { createAPI } from '@/utils/request'
+
+// 目录列表
+// export const list = data => createAPI('/direcotrys', 'get', data)
+export const getDirectorys = data => createAPI('/directorys', 'get', data)
+
+// 目录简单列表
+export const getDirectorysById = id => createAPI(`/directorys/${id}`, 'get')
+export const simple = data => createAPI('/directorys/simple', 'get', data)
+
+// 目录详情
+export const detail = data =>
+  createAPI(`/direcotrys/${data.userId}`, 'get', data)
+
+// 目录添加
+// export const add = data => createAPI('/direcotrys', 'post', data)
+export const addDirectorys = data => createAPI('/directorys', 'post', data)
+
+// 目录修改
+// export const update = data => createAPI(`/direcotrys/${data.id}`, 'put', data)
+export const EditDirectorys = data => createAPI(`/directorys/${data.id}`, 'put', data)
+
+// 目录删除
+// export const remove = data =>
+//   createAPI(`/directorys/${data.id}`, 'delete', data)
+export const DelDirectorys = id => createAPI(`/directorys/${id}`, 'delete')
+
+// 目录状态
+// export const changeState = data => createAPI(`/directorys/${data.id}`, 'get')
+
+// 修改启动和禁用
+export const EditState = data => createAPI(`/directorys/${data.id}/${data.state}`, 'POST', data)
---
+++ src/api/hmmm/tags.js
@@ -0,0 +1,67 @@
+/*
+ * @Author: zhanglianchang
+ * @Date: 2020-08-01 14:39:32
+ * @Last Modified by: taoshiwei
+ * @Last Modified time: 2019-04-29 14:57:26
+ *
+ * 标签有关的接口：自己写吧~~  已经被张老师删了 ^_^
+ */
+import { request } from '@/utils/request'
+// 获取学科简单列表
+export function getSubjectsList (params) {
+  return request({
+    url: '/subjects/simple',
+    method: 'get',
+    params
+  })
+}
+
+export function getTagList (params) {
+  return request({
+    url: '/tags',
+    method: 'get',
+    params
+  })
+}
+// 增加标签
+export function addTag (data) {
+  return request({
+    url: '/tags',
+    method: 'post',
+    data
+  })
+}
+
+// 获取标签祥情
+export function getTagDetail (id) {
+  return request({
+    url: `/tags/${id}`,
+    method: 'get'
+  })
+}
+
+// 修改标签
+export function editeTag (data) {
+  return request({
+    url: `/tags/${data.id}`,
+    method: 'put',
+    data
+  })
+}
+
+// 删除标签
+export function delTag (id) {
+  return request({
+    url: `/tags/${id}`,
+    method: 'delete'
+  })
+}
+
+// 上传标签状态
+export function postTagState (data) {
+  return request({
+    url: `/tags/${data.id}/${data.state}`,
+    method: 'post',
+    data
+  })
+}
